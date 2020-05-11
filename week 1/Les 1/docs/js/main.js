692a1a6bfe6af26913ec3da6334cd714d89bbaeb
Bas <57452503+basekkelenkamp@users.noreply.github.com>
2020-05-11 14:14:28 +0200

    first commit

---
+++ week 1/Les 1/docs/js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", () => {
+    let game = document.getElementsByTagName("game")[0];
+    let element = document.createElement("ball");
+    game.appendChild(element);
+    let ball = {
+        x: 100,
+        y: 100,
+        color: "Green"
+    };
+    ball.color = "Blue";
+    gameLoop();
+    function gameLoop() {
+        ball.x = ball.x + 2;
+        ball.y += 1;
+        element.style.transform = `translate(${ball.x}px, ${ball.y}px)`;
+        element.style.backgroundColor = ball.color;
+        requestAnimationFrame(gameLoop);
+    }
+});
+//# sourceMappingURL=main.js.map
